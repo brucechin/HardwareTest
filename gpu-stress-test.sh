474e28d440386887912f97d5f820f19853db585f
lianke <1026193951@sjtu.edu.cn>
2018-04-27 18:32:50 +0800

    修改gpu-stress-test 输出格式，方便查看各个gpu的温度

--- gpu-stress-test.sh
+++ gpu-stress-test.sh
@@ -2,7 +2,7 @@
 
 cd result
 touch gpu-stress-test.txt
-cd ../gpu-burn
+cd ../gpu-stress-test
 echo "Doing GPU stress test,result is stored in ./result/gpu-stress-test.txt"
 ./gpu_burn 500000  > ../result/gpu-stress-test.txt
 echo "GPU stress test done"
